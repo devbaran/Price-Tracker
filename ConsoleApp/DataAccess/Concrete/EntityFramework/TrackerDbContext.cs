05bcbffc4886cb1a487b385ac9809c00c6ad982c
Baran Kurtbogan <barankurtbogan@gmail.com>
2022-05-09 05:30:50 +0300

    Last update

--- ConsoleApp/DataAccess/Concrete/EntityFramework/TrackerDbContext.cs
+++ ConsoleApp/DataAccess/Concrete/EntityFramework/TrackerDbContext.cs
@@ -7,7 +7,8 @@ namespace DataAccess.Concrete.EntityFramework
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"YOUR_CONNECTION_STRING");
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=ShoppingDatabase;Trusted_Connection=true");
+            //optionsBuilder.UseSqlServer(@"YOUR_CONNECTION_STRING");
         }
 
         public DbSet<UserProductTracker> UserProductTrackers { get; set; }
